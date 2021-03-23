37de11a35f39d431842e5bd58a1d25cceebba83e
Libo Jian <1220266538@qq.com>
2021-03-23 11:35:59 -0400

    change port

--- server.py
+++ server.py
@@ -716,6 +716,34 @@ def user():
 def error():
     return 'ERROR'
 
-app.run()
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=5000, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using:
+
+        python server.py
+
+    Show the help text using:
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    #print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
+
+# app.run()
 
 
